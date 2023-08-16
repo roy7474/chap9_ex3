321b069de95d90445c67a4c9daa8714d1bfc85be
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-16 15:09:52 -0400

    extra program has been added towards the end of the program to scan all of the emails, regardless whether these were sent or received

--- Chap9_ex3.py
+++ Chap9_ex3.py
@@ -3,7 +3,8 @@ to count how many messages have come from each email address, and print the dict
 fhand = open('mbox-short.txt')
 
 #create the dictionary
-emails = dict()
+emails_received = dict()
+all_emails = dict()
 '''
 #get the file to be opened
 try:
@@ -13,38 +14,44 @@ except:
     print('There was a problem while trying to open the file, please confirm the name of the file and try again!')
     exit()
 
+
 #open text file
 fhand = open(file_name)
-
-for line in fhand:
-    # split the lines into words
-    words=line.split()
-
-    #find the words that start with From
-    if len(words) < 2 or words[0] != 'From':
-        continue
-    else:
-        if words[1] not in emails:
-            emails[words[1]] = 1
-
-        else:
-            emails[words[1]] +=1
-
-#unescessary since it is more advanced, but more organized way to print the dict content
-for key, value in emails.items():
-    print(f"{key}: {value}")
 '''
 
+#
+for line in fhand:
+    word = line.split()
+    if len(word) < 2 or word[0] != 'From': 
+        continue
+    else:
+        if word[1] not in emails_received:
+            emails_received[word[1]] = 1
 
+        else:
+            emails_received[word[1]] +=1
+
+
+print('This part of the program prints all of the emails received in the text file: ')
+#unescessary since it is more advanced, but more organized way to print the dict content
+for key, value in emails_received.items():
+    print(f"{key}: {value}")
+print('-------------------------- End of program 1 -------------')
+print(word)
+# Extra program
+# This part of the program scans the entire text file
+
+
+fhand = open('mbox-short.txt')
 read_file = fhand.read().split()
+#print(read_file)
 for word in read_file:
     # split the lines into words
-    #words=line.split()
-
+    
     if '@' in word:
-         emails[word] = emails.get(word, 0) + 1
-
-for word, count in emails.items():
-    print(f'{word}: {count}')
+         all_emails[word] = all_emails.get(word, 0) + 1
+print('This part of the program prints all of the emails found in the text file:')
 
 
+for word, count in all_emails.items():
+    print(f'{word}: {count}') 
